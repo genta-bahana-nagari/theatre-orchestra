b6704d1886f22ba7f96321811e533b427c01c231
Genta <gentapossenti@gmail.com>
2025-07-10 10:19:04 +0700

    new static data

--- src/data/broadway-shows.ts
+++ src/data/broadway-shows.ts
@@ -17,4 +17,28 @@ export const broadwayShows = [
       "https://www.broadway.com/static/27d97b8886cd108b6405e7c9b3ffdc94/1f64e/wicked-400x600.jpg",
     link: "https://www.broadway.com/shows/wicked/",
   },
+  {
+    title: "Hamilton",
+    image:
+      "https://www.broadway.com/static/74f28e429080da8e33ed08b1df3cf997/1f64e/hamilton-400x600.jpg",
+    link: "https://www.broadway.com/shows/hamilton-broadway/",
+  },
+  {
+    title: "Wicked",
+    image:
+      "https://www.broadway.com/static/27d97b8886cd108b6405e7c9b3ffdc94/1f64e/wicked-400x600.jpg",
+    link: "https://www.broadway.com/shows/wicked/",
+  },
+  {
+    title: "Hamilton",
+    image:
+      "https://www.broadway.com/static/74f28e429080da8e33ed08b1df3cf997/1f64e/hamilton-400x600.jpg",
+    link: "https://www.broadway.com/shows/hamilton-broadway/",
+  },
+  {
+    title: "Wicked",
+    image:
+      "https://www.broadway.com/static/27d97b8886cd108b6405e7c9b3ffdc94/1f64e/wicked-400x600.jpg",
+    link: "https://www.broadway.com/shows/wicked/",
+  },
 ];
